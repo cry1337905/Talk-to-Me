854e6ca7d6ca02c820452ebbc95526639432e792
cry1337905 <matze_abraxas@hotmail.de>
2026-07-29 22:15:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 users = {}
 
